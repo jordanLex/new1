9f202b245c1087bc1aca37957726011dbb372c9f
jordanLex <jordan--92@mail.ru>
2014-01-29 10:12:13 +0200

    Додав можливість перегляду веб сторінок
    тільки на одній вкладнці

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -15,5 +15,21 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode.ToString() == "return");
+            if (e.KeyCode==Keys.Enter)
+            {
+                System.IO.StreamWriter sw;
+                System.IO.FileInfo fi = new System.IO.FileInfo("name.txt");
+                sw = fi.AppendText();
+                sw.WriteLine(textBox1.Text);
+                sw.Close();
+                tabs.SelectedTab.Text = textBox1.Text;
+                ((WebBrowser)tabs.SelectedTab.Controls[0]).Navigate(@"http://www." + textBox1.Text);
+                
+            }
+        }
     }
 }
